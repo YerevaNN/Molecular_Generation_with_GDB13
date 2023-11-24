4bf82049cd930217fc58525ef0438abd6ad6e12c
knarik1 <knarik.mh@gmail.com>
2023-11-24 14:06:28 +0400

    Structure run templates.

--- smiles2selfies.py
+++ smiles2selfies.py
@@ -1,15 +1,20 @@
-import selfies as sf
+import os
 import json
+import selfies as sf
 from tqdm import tqdm
 
 
 if __name__ == "__main__":
-    # substr = "sas_7"
-    substr = "original"
+    subset_name = "equal_dist"
 
     for split in ["train", "valid"]:
-        with open(f"./data-bin/data-subsets/{split}_{substr}.jsonl", "r") as file_1:
-            with open(f"./data-bin/data-subsets/{split}_{substr}_selfies.jsonl", "w") as file_2:
+        folder_path = f"./data/data_bin_{subset_name}_sf_1000K/{split}/00/"
+
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        with open(f"./data/data_bin_{subset_name}_sm_1000K/{split}/00/{split}_{subset_name}_smiles.jsonl", "r") as file_1:
+            with open(f"./data/data_bin_{subset_name}_sf_1000K/{split}/00/{split}_{subset_name}_smiles.jsonl", "w") as file_2:
                 for line_str in tqdm(file_1):
                     line_obj = json.loads(line_str)
                     # Transform to selfies
